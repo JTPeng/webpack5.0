f321e4d54903c2ef54bcc4fa3b3b9a39cccaf60e
JTPeng <404502753@qq.com>
2020-11-05 14:46:03 +0800

    压缩css

---
+++ 11.webpack-压缩css/webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const { resolve } = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'js/index.js',
+    path: resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [require('postcss-preset-env')()],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/index.css',
+    }),
+    new OptimizeCssAssetsPlugin(),
+  ],
+}
