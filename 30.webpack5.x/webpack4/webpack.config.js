df1b000a8a23b6ddc6c9ca0fcfb4baedc7342e45
JTPeng <404502753@qq.com>
2020-11-10 10:05:54 +0800

    webpack4.x和webpack5.x

---
+++ 30.webpack5.x/webpack4/webpack.config.js
@@ -0,0 +1,10 @@
+const { resolve } = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'js/index.js',
+    path: resolve(__dirname, 'dist'),
+  },
+  mode: 'production',
+}
